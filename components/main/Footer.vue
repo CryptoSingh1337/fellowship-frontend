5a80d8867598dfec65212c799c476d6fbaabc049
Saransh Kumar <cryptosingh@gmail.com>
2022-08-06 10:59:23 +0530

    Setup layout

---
+++ components/main/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} — <strong>{{ title }}</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+};
+</script>
