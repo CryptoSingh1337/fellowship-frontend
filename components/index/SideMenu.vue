2488f47745029f588b6fe1e4c5443eb566a32a2c
Saransh Kumar <cryptosingh@gmail.com>
2022-08-14 12:16:28 +0530

    Add data fetching functionality

--- components/index/SideMenu.vue
+++ components/index/SideMenu.vue
@@ -12,7 +12,8 @@
       <h4 class="">Degrees:</h4>
       <v-row justify="center" no-gutters>
         <v-col cols="10">
-          <v-checkbox v-for="(d, idx) in degrees" :key="idx" v-model="degree" :label="d" :value="d" hide-details/>
+          <v-checkbox v-for="(d, idx) in degrees" :key="idx" v-model="degree" :label="d.name" :value="d.value"
+                      hide-details/>
         </v-col>
       </v-row>
     </v-row>
@@ -20,28 +21,47 @@
     <v-row justify="center" no-gutters>
       <v-col class="d-flex justify-space-between" cols="10">
         <v-btn class="elevation-0" color="gray" outlined tile @click.prevent="country = []; degree = [];">Reset</v-btn>
-        <v-btn class="elevation-0" color="primary" tile>Apply</v-btn>
+        <v-btn class="elevation-0" color="primary" tile @click.prevent="$nuxt.$emit('applyFilter');">Apply</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import countries from "countries-list";
+import Loader from "@/components/main/Loader.vue";
 
 export default {
   name: "SideMenu",
+  components: {
+    Loader
+  },
+  props: {
+    countries: Array
+  },
   data: () => ({
-    country: "",
+    country: [],
     degree: [],
-    degrees: ["Bachelor", "Master", "Phd"],
-    countries: Object.keys(countries.countries).map(code => countries.countries[code].name).sort()
-  })
+    degrees: [
+      {
+        name: "Bachelor",
+        value: "BACHELOR"
+      },
+      {
+        name: "Master",
+        value: "MASTER"
+      },
+      {
+        name: "Phd",
+        value: "PHD"
+      }],
+  }),
+  watch: {
+    country() {
+      this.$nuxt.$emit("changeCountry", this.country);
+    },
+    degree() {
+      this.$nuxt.$emit("changeDegree", this.degree);
+    }
+  }
 }
 </script>
-
-<style scoped>
-.border {
-  border: 1px solid grey;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-container class="fill-height" fluid>
+  <Loader v-if="$fetchState.pending"/>
+  <v-container v-else class="fill-height" fluid>
     <v-row align="center" class="pt-10" justify="center">
       <v-col cols="12" md="8">
         <v-text-field v-model="searchQuery" append-icon="mdi-magnify" class="mb-10" hide-details label="Search"
-                      outlined/>
+                      :loading="searchLoading" outlined @click:append="search" @keydown.enter="search"/>
         <v-row class="ma-3" no-gutters>
           <v-col cols="12" md="3">
             <div class="d-flex">
-              <SideMenu/>
+              <SideMenu :countries="countries"/>
               <v-divider v-if="$vuetify.breakpoint.mdAndUp" vertical/>
             </div>
             <v-divider v-if="$vuetify.breakpoint.smAndDown" class="my-6"/>
@@ -15,10 +16,16 @@
           <v-col :class="$vuetify.breakpoint.mdAndUp ? 'ml-5' : ''">
             <h2 :class="$vuetify.breakpoint.smAndDown ? 'text-center' : ''">
               {{ $auth ? ($auth.loggedIn ? 'Recommended' : 'Latest') : 'Latest' }} Scholarships</h2>
-            <ScholarshipCard v-for="i in new Array(10)" :key="i" :idx="i"/>
-            <div class="d-flex justify-center">
-              <v-btn v-model="page" class="elevation-0" color="primary" large tile @click.prevent="page++">Load more
-              </v-btn>
+            <v-container v-if="this.scholarships.length === 0" class="flex-column fill-height justify-center" fluid>
+              <v-icon color="" large>mdi-alert-circle-outline</v-icon>
+              <h3 class="title">No Scholarship found</h3>
+            </v-container>
+            <div v-else>
+              <ScholarshipCard v-for="(s, idx) in scholarships" :key="idx" :idx="idx" :scholarship="s"/>
+              <div class="d-flex justify-center">
+                <v-btn v-model="page" class="elevation-0" color="primary" large tile @click.prevent="page++">Load more
+                </v-btn>
+              </div>
             </div>
           </v-col>
         </v-row>
@@ -28,6 +35,7 @@
 </template>
 
 <script>
+import Loader from "@/components/main/Loader.vue";
 import SideMenu from "@/components/index/SideMenu.vue";
 import ScholarshipCard from "@/components/card/Scholarship.vue";
 
@@ -36,14 +44,63 @@ export default {
     title: "Home - Fellowship"
   }),
   components: {
+    Loader,
     SideMenu,
     ScholarshipCard
   },
   data: () => ({
     searchQuery: "",
+    searchLoading: false,
+    scholarships: [],
+    country: [],
+    degree: [],
+    countries: [],
     page: 0,
-  })
+  }),
+  created() {
+    this.$nuxt.$on('changeCountry', (payload) => this.country = payload);
+    this.$nuxt.$on('changeDegree', (payload) => this.degree = payload);
+    this.$nuxt.$on('applyFilter', () => this.search());
+  },
+  methods: {
+    search() {
+      this.searchLoading = true;
+      const filters = {
+        degrees: this.degree,
+        countries: this.country,
+        search: this.searchQuery,
+        page: this.page
+      };
+      this.$axios.post("/scholarship/get", filters)
+        .then(response => response.data.data)
+        .then(data => this.scholarships = data.scholarships)
+        .then(() => this.searchLoading = false)
+        .catch(err => console.log(err))
+    }
+  },
+  async fetch() {
+    let response = await this.$axios.get("/scholarship/get/countries");
+    if (response.status === 200)
+      this.countries = await response.data.data.countries.sort();
+
+    if (this.$auth && this.$auth.loggedIn) {
+      const {country, program} = this.$auth.user;
+      if (country && program) {
+        const filters = {
+          degrees: [program],
+          countries: [country],
+          search: null,
+          page: this.page
+        };
+        response = await this.$axios.post("/scholarship/search", filters);
+      } else {
+        response = await this.$axios.get(`/scholarship/all?page=${this.page}`);
+      }
+    } else {
+      response = await this.$axios.get(`/scholarship/all?page=${this.page}`)
+    }
+    if (response.status === 200)
+      this.scholarships = await response.data.data.scholarships;
+  }
 };
 </script>
-<style>
-</style>
